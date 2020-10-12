56091565fdfb0da38995ad5455381159e08e63b8
RAFIQ JALLEL <34679232+rafiqips5@users.noreply.github.com>
2020-10-12 13:12:36 +0700

    UIT and SIT passed code

---
+++ FileUploader_2C2P_API/Controllers/TransactionController.cs
@@ -0,0 +1,130 @@
+﻿using FileUploader_2C2P.Helper;
+using FileUploader_2C2P_API.Helper;
+using FileUploader_2C2P_API.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+using FileUploader_2C2P_API.Models;
+
+namespace FileUploader_2C2P_API.Controllers
+{
+    public class TransactionController : ApiController
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        DatabaseHelper databaseHelper = null;
+        // GET api/<controller>
+        [Route("api/Transaction/TransactionByCurrency/{currency}")]
+        [HttpGet]
+        public HttpResponseMessage GetTransactionByCurrency(string currency)
+        {
+            databaseHelper = new DatabaseHelper();
+            if (string.IsNullOrEmpty(currency))
+            {
+                log.InfoFormat("currnecy is empty");
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ErrorMessages.NotFoundCurrency);
+            }
+            if (currency.Length > 3)
+            {
+                log.InfoFormat("Bad input {0}", currency);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ErrorMessages.BadInput);
+            }
+            List<GetTransactionResponse> getTransactionResponse = databaseHelper.GetTransaactionByCurrencyType(currency);
+            return Request.CreateResponse(getTransactionResponse);
+        }
+        [HttpGet]
+        [Route("api/Transaction/TransactionByStatus/{status}")]
+        public HttpResponseMessage GetTransactionByStatus(string status)
+        {
+            databaseHelper = new DatabaseHelper();
+            string[] statusArray = new string[3];
+            string[] statusXMLArray = new string[3];
+            List<GetTransactionResponse> getTransactionResponse = null;
+
+            try
+            {
+                if (string.IsNullOrEmpty(status))
+                {
+                    log.InfoFormat("status is empty");
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, ErrorMessages.NotFoundStatusDesc);
+                }
+                if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["CSVStatus"]) && (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["XMLStatus"])))
+                {
+                    statusArray = ConfigurationManager.AppSettings["CSVStatus"].Split(',');
+                    statusXMLArray = ConfigurationManager.AppSettings["XMLStatus"].Split(',');
+                    if (statusArray.Contains(status.ToLower()) || statusXMLArray.Contains(status.ToLower()))
+                    {
+                        log.InfoFormat("Status : {0} is valid", status);
+                        getTransactionResponse = databaseHelper.GetTransaactionBySatus(status);
+                        return Request.CreateResponse(getTransactionResponse);
+                    }
+                    else
+                    {
+                        log.InfoFormat("Invalid status Input {0}", status);
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ErrorMessages.InvalidStatusDesc);
+                    }
+                }
+                else
+                {
+                    log.InfoFormat("Invalid Configuration. Status in missing in appSeetings section");
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+                }
+            }
+            catch (WebException ex)
+            {
+                log.ErrorFormat("Web exception occured {0} ", ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Unhandled exception occured {0} ", ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+
+        }
+        [HttpPost]
+        [Route("api/Transaction/TransactionByDate")]
+        public HttpResponseMessage GetTransactionByDate([FromBody] GetTransactionByDatesRequest getTransactionByDatesRequest)
+        {
+            databaseHelper = new DatabaseHelper();
+            string[] statusArray = new string[3];
+            List<GetTransactionResponse> getTransactionResponse = null;
+
+            // DateTime stDate = DateTime.ParseExact(getTransactionByDatesRequest.startDate, "yyyy-mm-dd HH:mm:ss.tt", null);
+            //DateTime toDate = DateTime.ParseExact(getTransactionByDatesRequest.toDateTime, "yyyy-mm-dd HH:mm:ss.tt", null);
+            try
+            {
+                if (string.IsNullOrEmpty(getTransactionByDatesRequest.StartDate.ToString()) && string.IsNullOrEmpty(getTransactionByDatesRequest.EndDate.ToString()))
+                {
+                    log.InfoFormat("start date and end date is empty");
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, ErrorMessages.NotFoundDateTime);
+                }
+                if (getTransactionByDatesRequest.StartDate <= getTransactionByDatesRequest.StartDate)
+                {
+                    log.InfoFormat("start date : {0} and endDate {1} is valid", getTransactionByDatesRequest.StartDate, getTransactionByDatesRequest.EndDate);
+                    getTransactionResponse = databaseHelper.GetTransaactionByDate(getTransactionByDatesRequest.StartDate, getTransactionByDatesRequest.EndDate);
+                    return Request.CreateResponse(getTransactionResponse);
+                }
+                else
+                {
+                    log.InfoFormat("start date : {0} and endDate {1} is valid", getTransactionByDatesRequest.StartDate, getTransactionByDatesRequest.EndDate);
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ErrorMessages.InvalidDateRange);
+                }
+            }
+            catch (WebException ex)
+            {
+                log.ErrorFormat("Web exception occured {0} ", ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Unhandled exception occured {0} ", ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ErrorMessages.InternalServerError);
+            }
+        }
+    }
+}
--- FileUploader_2C2P_API/Helper/DatabaseHelper.cs
+++ FileUploader_2C2P_API/Helper/DatabaseHelper.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using FileUploader_2C2P_API.Models.Response;
 
 namespace FileUploader_2C2P.Helper
 {
@@ -20,9 +21,9 @@ namespace FileUploader_2C2P.Helper
                 sqlCon = ConfigurationManager.AppSettings["SqlCon"];
                 destinationTableName = ConfigurationManager.AppSettings["DestinationTableName"];
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                log.ErrorFormat("Error occured while fetching data from configuraiton {0}",ex);
+                log.ErrorFormat("Error occured while fetching data from configuraiton {0}", ex);
             }
         }
 
@@ -70,24 +71,167 @@ namespace FileUploader_2C2P.Helper
             return true;
         }
 
+        public List<GetTransactionResponse> GetTransaactionByCurrencyType(string currencyType)
+        {
+            log.Info("Get Transaction from database started");
+            List<GetTransactionResponse> getTransactionResponsesList = null;
+            string[] statusDesc = ConfigurationManager.AppSettings["XMLStatus"].Split(',');
+            try
+            {
+                if (ValidateDatabaseConfiguration())
+                {
+                    using (SqlConnection sqlConnection = new SqlConnection(sqlCon))
+                    {
+                        using (SqlCommand sqlCommand = new SqlCommand("PROC_GET_TRANSACTION_BY_CURRENCY", sqlConnection))
+                        {
+                            sqlConnection.Open();
+                            sqlCommand.CommandType = CommandType.StoredProcedure;
+                            sqlCommand.Parameters.AddWithValue("@i_CurrencyCode", currencyType);
+                            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+
+                            getTransactionResponsesList = new List<GetTransactionResponse>();
+                            while (sqlDataReader.Read())
+                            {
+                                GetTransactionResponse getTransactionResponse = new GetTransactionResponse();
+                                getTransactionResponse.Id = sqlDataReader["TransactionIdentificator"].ToString();
+                                getTransactionResponse.Payment = string.Format("{0} {1}", sqlDataReader["Amount"], sqlDataReader["CurrencyCode"]);
+                                string status = sqlDataReader["Status"].ToString();
+                                getTransactionResponse.Status = GetStatusMappingsForDescription(status);
+                                getTransactionResponsesList.Add(getTransactionResponse);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Un handled exception occured : {0}", ex);
+                return getTransactionResponsesList;
+            }
+            return getTransactionResponsesList;
+        }
+
+        public List<GetTransactionResponse> GetTransaactionBySatus(string status)
+        {
+            log.Info("Get Transaction from database started");
+            List<GetTransactionResponse> getTransactionResponsesList = null;
+            try
+            {
+                if (ValidateDatabaseConfiguration())
+                {
+                    using (SqlConnection sqlConnection = new SqlConnection(sqlCon))
+                    {
+                        using (SqlCommand sqlCommand = new SqlCommand("PROC_GET_TRANSACTION_BY_STATUS", sqlConnection))
+                        {
+                            sqlConnection.Open();
+                            sqlCommand.CommandType = CommandType.StoredProcedure;
+                            sqlCommand.Parameters.AddWithValue("@i_STATUS", status);
+                            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+
+                            getTransactionResponsesList = new List<GetTransactionResponse>();
+                            while (sqlDataReader.Read())
+                            {
+                                GetTransactionResponse getTransactionResponse = new GetTransactionResponse();
+                                getTransactionResponse.Id = sqlDataReader["TransactionIdentificator"].ToString();
+                                getTransactionResponse.Payment = string.Format("{0} {1}", sqlDataReader["Amount"], sqlDataReader["CurrencyCode"]);
+
+                                string statusDesc = sqlDataReader["Status"].ToString();
+                                getTransactionResponse.Status = GetStatusMappingsForDescription(statusDesc);
+                                getTransactionResponsesList.Add(getTransactionResponse);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Un handled exception occured : {0}", ex);
+                return getTransactionResponsesList;
+            }
+            return getTransactionResponsesList;
+        }
+
+        public List<GetTransactionResponse> GetTransaactionByDate(DateTime startDate, DateTime endDate)
+        {
+            log.Info("Get Transaction from database started");
+            List<GetTransactionResponse> getTransactionResponsesList = null;
+            try
+            {
+                if (ValidateDatabaseConfiguration())
+                {
+                    using (SqlConnection sqlConnection = new SqlConnection(sqlCon))
+                    {
+                        using (SqlCommand sqlCommand = new SqlCommand("PROC_GET_TRANSACTION_BY_DATE", sqlConnection))
+                        {
+                            sqlConnection.Open();
+                            sqlCommand.CommandType = CommandType.StoredProcedure;
+                            sqlCommand.Parameters.AddWithValue("@i_StartDate", startDate);
+                            sqlCommand.Parameters.AddWithValue("@i_EndDate", startDate);
+                            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+
+                            getTransactionResponsesList = new List<GetTransactionResponse>();
+                            while (sqlDataReader.Read())
+                            {
+                                GetTransactionResponse getTransactionResponse = new GetTransactionResponse();
+                                getTransactionResponse.Id = sqlDataReader["TransactionIdentificator"].ToString();
+                                getTransactionResponse.Payment = string.Format("{0} {1}", sqlDataReader["Amount"], sqlDataReader["CurrencyCode"]);
+                                string status = sqlDataReader["Status"].ToString();
+                                getTransactionResponse.Status = GetStatusMappingsForDescription(status);
+                                getTransactionResponsesList.Add(getTransactionResponse);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("Un handled exception occured : {0}", ex);
+                return getTransactionResponsesList;
+            }
+            return getTransactionResponsesList;
+        }
+
         public bool ValidateDatabaseConfiguration()
         {
             log.Info("Validating Configuration");
             try
             {
-                if(!string.IsNullOrEmpty(sqlCon) &&  !string.IsNullOrEmpty(destinationTableName))
+                if (!string.IsNullOrEmpty(sqlCon) && !string.IsNullOrEmpty(destinationTableName))
                 {
                     log.InfoFormat("Validation Configuration successfully");
                     return true;
                 }
             }
 
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                log.ErrorFormat("Exception Occured while fetching the data from configuraiton {0}",ex);
+                log.ErrorFormat("Exception Occured while fetching the data from configuraiton {0}", ex);
                 return false;
             }
             return false;
+        }
+
+        public char GetStatusMappingsForDescription(string status)
+        {
+            char charStatus;
+            if (status.ToLower() == "failed")
+            {
+                charStatus = 'R';
+            }
+            else if (status.ToLower() == "fineshed")
+            {
+                charStatus = 'D';
+            }
+            else
+            {
+                charStatus = Convert.ToChar(status.ToString().Substring(0, 1).ToUpper());
+            }
+
+            return charStatus;
         }
     }
 }
